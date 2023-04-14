60ad85b2f205fc7e7d42f8f7248ace0c01ed0b79
Julian Schnitzler <julian.schnitzler@yahoo.de>
2023-04-14 12:41:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 function HelloWorld() {
@@ -5,28 +6,51 @@ function HelloWorld() {
 }
 
 function Card(props: any) {
-  const label = props.label;
-  return <div className="Card">{label}</div>;
+  const label = props.shown ? props.label : "";
+  return (
+    <div className="Card" onClick={props.onClick}>
+      {label}
+    </div>
+  );
 }
 
 function Field(props: any) {
+  const [shown, setShown] = useState<boolean[]>(Array(8).fill(false));
+
+  function handleClick(i: number) {
+    function toggleShown(previous: boolean[]) {
+      const arr = [...previous];
+      arr[i] = !arr[i];
+      return arr;
+    }
+    setShown(toggleShown);
+  }
+
+  const arr = Array(8)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .map((num) => (
+      <Card
+        label={`Card ${num}`}
+        shown={shown[num - 1]}
+        key={num}
+        onClick={() => handleClick(num - 1)}
+      />
+    ));
+
   return (
     <div className="Field">
       <h1>Memory</h1>
-      <div className="CardHolder">{props.arr}</div>
+      <div className="CardHolder">{arr}</div>
     </div>
   );
 }
 
 export default function App() {
-  const arr = Array(8)
-    .fill(0)
-    .map((_, i) => i + 1)
-    .map((num) => <Card label={`Card ${num}`} />);
   return (
     <div className="App">
       <h1>A game of</h1>
-      <Field arr={arr} />
+      <Field />
     </div>
   );
 }
