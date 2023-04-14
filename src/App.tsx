1e60c72aafdfb0e51fb74dc137153a3dfe5c0081
Julian Schnitzler <32015957+t0asty@users.noreply.github.com>
2023-04-14 10:06:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+function HelloWorld() {
+  return <div>Hello World</div>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <HelloWorld />
+    </div>
+  );
+}
